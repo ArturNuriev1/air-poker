d840aac7fdb9155a5487f0f8e5d77991c18a856f
ArturNuriev1 <112039058+ArturNuriev1@users.noreply.github.com>
2023-12-08 23:06:21 +1100

    fixes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http').createServer(server)
 const io = require('socket.io')(http, {
     cors: {
       origin: '*',
-    }
+    }, 'pingTimeout': 1000 * 60 * 5, 'pingInterval': 1000 * 60 * 3
   })
 
 let players = []
