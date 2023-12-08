10ef6fb0f8b366619640151cceb4bf923c3601ab
ArturNuriev1 <112039058+ArturNuriev1@users.noreply.github.com>
2023-12-09 00:13:04 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const server = require('express')()
 const http = require('http').createServer(server)
 const io = require('socket.io')(http, {
+    transports: ['websocket'],
     cors: {credentials: true, origin: true}, 
     'pingTimeout': 1000 * 60 * 5, 
     'pingInterval': 1000 * 60 * 3
