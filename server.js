c5ae4c3d3b80af647711e8f3285a072bab3f1b23
ArturNuriev1 <112039058+ArturNuriev1@users.noreply.github.com>
2023-12-09 14:57:29 +1100

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,10 @@
 
 const express = require('express')
 const http = require('http')
-const path = require('path')
 const socketIO = require('socket.io')
 
 const app = express()
-var server = http.Server(app)
+var server = http.createServer(app)
 var io = socketIO(server, {
   pingTimeout: 60000,
 })
@@ -22,9 +21,6 @@ var io = socketIO(server, {
 app.set('port', 5000)
 app.use('/static', express.static(__dirname + '/static'))
 
-app.get('/', function (request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'))
-})
 
 server.listen(5000, function () {
   console.log('Starting server on port 5000')
