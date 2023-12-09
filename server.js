82c7cd5c39d533470c408e33f88527e3d0c9729c
ArturNuriev1 <112039058+ArturNuriev1@users.noreply.github.com>
2023-12-09 14:23:48 +1100

    test

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
-const server = require('express')()
-const http = require('http').createServer(server)
-const io = require('socket.io')(http, {
-    cors: {credentials: true, origin: true}, 
-    maxDisconnectionDuration: 13 * 60 * 1000,
-    pingInterval: 13 * 60 * 1000,
-    pingTimeout: 13 * 60 * 1000,
-    // transports: ['websocket']
+// const server = require('express')()
+// const http = require('http').createServer(server)
+// const io = require('socket.io')(http, {
+//     cors: {credentials: true, origin: true}, 
+//     maxDisconnectionDuration: 13 * 60 * 1000,
+//     pingInterval: 13 * 60 * 1000,
+//     pingTimeout: 13 * 60 * 1000,
+//     // transports: ['websocket']
+// })
+
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketIO = require('socket.io')
+
+const app = express()
+var server = http.Server(app)
+var io = socketIO(server, {
+  pingTimeout: 60000,
 })
+
+app.set('port', 5000)
+app.use('/static', express.static(__dirname + '/static'))
+
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'))
+})
+
+server.listen(5000, function () {
+  console.log('Starting server on port 5000')
+})
+
 
 let players = []
 let pickedVals = []
@@ -117,8 +140,8 @@ io.on('connect', function (socket) {
     })
 })
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-http.listen(port, function () {
-    console.log('Server started!')
-})
+// http.listen(port, function () {
+//     console.log('Server started!')
+// })
